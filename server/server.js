d827e41dba26659999f56b5123868ba6193bf9bd
Jordan French <jtf981@culver.edu>
2023-02-09 15:14:00 -0600

    initial server setup

---
+++ server/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import bodyParser from "bodyParser";
+import mongoose from "mongoose";
+import helmet from "helmet";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import gameRoutes from "./routes/games.js";
+
+// CONFIG
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// ROUTES
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/games", gameRoutes);
+
+// Mongoose Config
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
