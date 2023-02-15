72b023d87a525daa17df09cdd526ef281e14ceba
Jordan French <jtf981@culver.edu>
2023-02-15 11:35:17 -0600

    eliminated use of state for games because it caused incorrect rendering of stats from previous page

--- client/src/functions/games.js
+++ client/src/functions/games.js
@@ -17,32 +17,96 @@ function Averages(data){
     var totalFoulsLoss=0;
     let winCount= 0;
     let lossCount=0;
+    var totalPointsPark = 0;
+    var totalAssistsPark=0;
+    var totalStealsPark=0;
+    var totalReboundsPark=0;
+    var totalTurnoversPark=0;
+    var totalBlocksPark=0;
+    var totalFoulsPark=0;
+    var totalPointsLossPark = 0;
+    var totalAssistsLossPark=0;
+    var totalStealsLossPark=0;
+    var totalReboundsLossPark=0;
+    var totalTurnoversLossPark=0;
+    var totalBlocksLossPark=0;
+    var totalFoulsLossPark=0;
+    let winCountPark= 0;
+    let lossCountPark=0;
     while (i<data.length){
-        console.log(data[i]);
-        if(data[i].isWin===true){
-            totalPoints+=data[i].points;
-            totalAssists+=data[i].assists;
-            totalSteals+=data[i].steals;
-            totalBlocks+=data[i].blocks
-            totalRebounds+=data[i].rebounds;
-            totalTurnovers+=data[i].turnovers;
-            totalFouls+=data[i].fouls;
-            winCount+=1;
-        }else{
-            console.log(data[i]);
-            totalPointsLoss+=data[i].points;
-            totalAssistsLoss+=data[i].assists;
-            totalStealsLoss+=data[i].steals;
-            totalBlocksLoss+=data[i].blocks
-            totalReboundsLoss+=data[i].rebounds;
-            totalTurnoversLoss+=data[i].turnovers;
-            totalFoulsLoss+=data[i].fouls;
-            lossCount+=1;
-        };
+        if(data[i].isRecGame===true){
+            if(data[i].isWin===true){
+                totalPoints+=data[i].points;
+                totalAssists+=data[i].assists;
+                totalSteals+=data[i].steals;
+                totalBlocks+=data[i].blocks
+                totalRebounds+=data[i].rebounds;
+                totalTurnovers+=data[i].turnovers;
+                totalFouls+=data[i].fouls;
+                winCount+=1;
+            }else{
+                totalPointsLoss+=data[i].points;
+                totalAssistsLoss+=data[i].assists;
+                totalStealsLoss+=data[i].steals;
+                totalBlocksLoss+=data[i].blocks
+                totalReboundsLoss+=data[i].rebounds;
+                totalTurnoversLoss+=data[i].turnovers;
+                totalFoulsLoss+=data[i].fouls;
+                lossCount+=1;
+                };
+        } else {
+                if(data[i].isWin===true){
+                    totalPointsPark+=data[i].points;
+                    totalAssistsPark+=data[i].assists;
+                    totalStealsPark+=data[i].steals;
+                    totalBlocksPark+=data[i].blocks
+                    totalReboundsPark+=data[i].rebounds;
+                    totalTurnoversPark+=data[i].turnovers;
+                    totalFoulsPark+=data[i].fouls;
+                    winCountPark+=1;
+                }else{
+                    totalPointsLossPark+=data[i].points;
+                    totalAssistsLossPark+=data[i].assists;
+                    totalStealsLossPark+=data[i].steals;
+                    totalBlocksLossPark+=data[i].blocks
+                    totalReboundsLossPark+=data[i].rebounds;
+                    totalTurnoversLossPark+=data[i].turnovers;
+                    totalFoulsLossPark+=data[i].fouls;
+                    lossCountPark+=1;
+                    };
+        }
         i++;
-        console.log(totalPoints);
     };
-    let value = {'pointsWin': (totalPoints/winCount).toFixed(2), 'assistsWin': (totalAssists/winCount).toFixed(2), 'stealsWin':(totalSteals/winCount).toFixed(2), 'blocksWin':(totalBlocks/winCount).toFixed(2), 'reboundsWin': (totalRebounds/winCount).toFixed(2), 'turnoversWin':(totalTurnovers/winCount).toFixed(2), 'foulsWin':(totalFouls/winCount).toFixed(2), 'pointsLoss': (totalPointsLoss/lossCount).toFixed(2), 'assistsLoss': (totalAssistsLoss/lossCount).toFixed(2), 'stealsLoss':(totalStealsLoss/lossCount).toFixed(2), 'blocksLoss':(totalBlocksLoss/lossCount).toFixed(2), 'reboundsLoss': (totalReboundsLoss/lossCount).toFixed(2), 'turnoversLoss':(totalTurnoversLoss/lossCount).toFixed(2), 'foulsLoss':(totalFoulsLoss/lossCount).toFixed(2)};
+    let value = {
+                    'pointsWin': (totalPoints/winCount).toFixed(2), 
+                    'assistsWin': (totalAssists/winCount).toFixed(2), 
+                    'stealsWin':(totalSteals/winCount).toFixed(2), 
+                    'blocksWin':(totalBlocks/winCount).toFixed(2), 
+                    'reboundsWin': (totalRebounds/winCount).toFixed(2), 
+                    'turnoversWin':(totalTurnovers/winCount).toFixed(2), 
+                    'foulsWin':(totalFouls/winCount).toFixed(2), 
+                    'pointsLoss': (totalPointsLoss/lossCount).toFixed(2), 
+                    'assistsLoss': (totalAssistsLoss/lossCount).toFixed(2), 
+                    'stealsLoss':(totalStealsLoss/lossCount).toFixed(2), 
+                    'blocksLoss':(totalBlocksLoss/lossCount).toFixed(2), 
+                    'reboundsLoss': (totalReboundsLoss/lossCount).toFixed(2), 
+                    'turnoversLoss':(totalTurnoversLoss/lossCount).toFixed(2), 
+                    'foulsLoss':(totalFoulsLoss/lossCount).toFixed(2),
+                    'pwp':(totalPointsPark/winCountPark).toFixed(2),
+                    'awp':(totalAssistsPark/winCountPark).toFixed(2),
+                    'swp':(totalStealsPark/winCountPark).toFixed(2),
+                    'bwp':(totalBlocksPark/winCountPark).toFixed(2),
+                    'rwp':(totalReboundsPark/winCountPark).toFixed(2),
+                    'twp':(totalTurnoversPark/winCountPark).toFixed(2),
+                    'fwp':(totalFoulsPark/winCountPark).toFixed(2),
+                    'plp':(totalPointsLossPark/lossCountPark).toFixed(2),
+                    'alp':(totalAssistsLossPark/lossCountPark).toFixed(2),
+                    'slp':(totalStealsLossPark/lossCountPark).toFixed(2),
+                    'blp':(totalBlocksLossPark/lossCountPark).toFixed(2),
+                    'rlp':(totalReboundsLossPark/lossCountPark).toFixed(2),
+                    'tlp':(totalTurnoversLossPark/lossCountPark).toFixed(2),
+                    'flp':(totalFoulsLossPark/lossCountPark).toFixed(2),
+                };
     return(value);
 };
 export default Averages;
--- client/src/scenes/widgets/gameStatsWidget.jsx
+++ client/src/scenes/widgets/gameStatsWidget.jsx
@@ -13,16 +13,14 @@ import {
     TableRow
 } from "@mui/material";
 import WidgetWrapper from "components/WidgetWrapper";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { setGames } from "state";
 
 const GameStatsWidget = ({id, isBuild}) => {
     console.log(id);
     console.log(isBuild);
-    const dispatch = useDispatch();
     const {palette}= useTheme();
-    const token = useSelector((state)=>state.token);
-    const games = useSelector((state)=>state.games);
+    const token = useSelector((state)=>state.token);;
     const [ppw, setPpw] = useState(0);
     const [apw, setApw]=useState(0);
     const [rpw, setRpw]=useState(0);
@@ -37,6 +35,20 @@ const GameStatsWidget = ({id, isBuild}) => {
     const [bpl, setBpl]=useState(0);
     const [tpl, setTpl]= useState(0);
     const [fpl, setFpl]=useState(0);
+    const [ppwp, setPpwp] = useState(0);
+    const [apwp, setApwp]=useState(0);
+    const [rpwp, setRpwp]=useState(0);
+    const [spwp, setSpwp]=useState(0);
+    const [bpwp, setBpwp]=useState(0);
+    const [tpwp, setTpwp]= useState(0);
+    const [fpwp, setFpwp]=useState(0);
+    const [pplp, setPplp] = useState(0);
+    const [aplp, setAplp]=useState(0);
+    const [rplp, setRplp]=useState(0);
+    const [splp, setSplp]=useState(0);
+    const [bplp, setBplp]=useState(0);
+    const [tplp, setTplp]= useState(0);
+    const [fplp, setFplp]=useState(0);
     
     function setAverages(data){
         const averages = Averages(data);
@@ -54,6 +66,21 @@ const GameStatsWidget = ({id, isBuild}) => {
         setBpl(averages.blocksLoss);
         setTpl(averages.turnoversLoss);
         setFpl(averages.foulsLoss);
+        setPpwp(averages.pwp);
+        setApwp(averages.awp);
+        setRpwp(averages.rwp);
+        setSpwp(averages.swp);
+        setBpwp(averages.bwp);
+        setTpwp(averages.twp);
+        setFpwp(averages.fwp);
+        setPplp(averages.plp);
+        setAplp(averages.alp);
+        setRplp(averages.rlp);
+        setSplp(averages.slp);
+        setBplp(averages.blp);
+        setTplp(averages.tlp);
+        setFplp(averages.flp);
+        return ('done');
     }
     const getGames = async ()=>{
         console.log('gettingGames');
@@ -67,8 +94,7 @@ const GameStatsWidget = ({id, isBuild}) => {
             );
             const data = await response.json();
             console.log(`game = ${data}`);
-            dispatch(setGames({games: data}));
-            setAverages(games);
+            setAverages(data);
         } else{
             const response = await fetch(
                 `http://localhost:3001/games/${id}/user_games`,
@@ -79,8 +105,7 @@ const GameStatsWidget = ({id, isBuild}) => {
             );
             const data = await response.json();
             console.log(`game = ${data}`);
-            dispatch(setGames({games: data}));
-            setAverages(games);
+            setAverages(data);
         }
     };
 
@@ -90,6 +115,14 @@ const GameStatsWidget = ({id, isBuild}) => {
 
     return (
         <WidgetWrapper>
+            <Typography
+                color={palette.neutral.dark}
+                variant="h2"
+                fontWeight="500"
+                sx={{mb:"1rem", textAlign:'center'}}
+            >
+                Rec Games
+            </Typography>
             <Typography
                 color={palette.primary.dark}
                 variant="h3"
@@ -159,7 +192,84 @@ const GameStatsWidget = ({id, isBuild}) => {
                     </TableBody>
                 </Table>
             </TableContainer>
-
+            <hr></hr>
+            <Typography
+                color={palette.neutral.dark}
+                variant="h2"
+                fontWeight="500"
+                sx={{mb:"1rem", mt:"1.5rem", textAlign:'center'}}
+            >
+                Park Games
+            </Typography>
+            <Typography
+                color={palette.primary.dark}
+                variant="h3"
+                fontWeight="500"
+                sx={{mb:"1.5rem"}}
+            >
+                Win Averages
+            </Typography>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Points/Win</TableCell>
+                            <TableCell align="center">Assists/Win</TableCell>
+                            <TableCell align="center">Steals/Win</TableCell>
+                            <TableCell align="center">Rebounds/Win</TableCell>
+                            <TableCell align="center">Blocks/Win</TableCell>
+                            <TableCell align="center">Turnovers/Win</TableCell>
+                            <TableCell align="center">Fouls/Win</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell align="center">{ppwp}</TableCell>
+                            <TableCell align="center">{apwp}</TableCell>
+                            <TableCell align="center">{spwp}</TableCell>
+                            <TableCell align="center">{rpwp}</TableCell>
+                            <TableCell align="center">{bpwp}</TableCell>
+                            <TableCell align="center">{tpwp}</TableCell>
+                            <TableCell align="center">{fpwp}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <hr></hr>
+            <Typography
+                color={palette.primary.dark}
+                variant="h3"
+                fontWeight="500"
+                sx={{mb:"1.5rem"}}
+            >
+                Loss Averages
+            </Typography>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Points/Loss</TableCell>
+                            <TableCell align="center">Assists/Loss</TableCell>
+                            <TableCell align="center">Steals/Loss</TableCell>
+                            <TableCell align="center">Rebounds/Loss</TableCell>
+                            <TableCell align="center">Blocks/Loss</TableCell>
+                            <TableCell align="center">Turnovers/Loss</TableCell>
+                            <TableCell align="center">Fouls/Loss</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell align="center">{pplp}</TableCell>
+                            <TableCell align="center">{aplp}</TableCell>
+                            <TableCell align="center">{splp}</TableCell>
+                            <TableCell align="center">{rplp}</TableCell>
+                            <TableCell align="center">{bplp}</TableCell>
+                            <TableCell align="center">{tplp}</TableCell>
+                            <TableCell align="center">{fplp}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </WidgetWrapper>
     )
 
